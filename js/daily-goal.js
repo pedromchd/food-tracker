937063a24cffc1403c8fbb8cefc618de05632d7f
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2025-01-21 03:42:02 +0000

    feat: add daily goal functionality with meal tracking and goal setting

---
+++ js/daily-goal.js
@@ -0,0 +1,46 @@
+$(function () {
+  window.weekMeals = [];
+
+  $("#add-day").on("click", function () {
+    const dayMeal = {
+      carboidratos: 0,
+      proteinas: 0,
+      gorduras: 0,
+    };
+
+    window.dailyMeals.forEach((meal) => {
+      dayMeal.carboidratos = sum(dayMeal.carboidratos, meal.carboidratos);
+      dayMeal.proteinas = sum(dayMeal.proteinas, meal.proteinas);
+      dayMeal.gorduras = sum(dayMeal.gorduras, meal.gorduras);
+    });
+
+    window.weekMeals.push(dayMeal);
+    clearDayMeals();
+  });
+
+  window.dailyGoal = {
+    carboidratos: 0,
+    proteinas: 0,
+    gorduras: 0,
+  };
+
+  $("#set-goal-btn").on("click", function () {
+    dailyGoal.carboidratos = parseFloat($("#carb-goal").val());
+    dailyGoal.proteinas = parseFloat($("#prot-goal").val());
+    dailyGoal.gorduras = parseFloat($("#fat-goal").val());
+  });
+});
+
+function clearDayMeals() {
+  window.dailyMeals = [];
+  $("#daily-meals tbody").html(`
+    <tr>
+      <th>Refeição</th>
+      <th>Carboidratos (g)</th>
+      <th>Proteínas (g)</th>
+      <th>Gorduras (g)</th>
+    </tr>
+  `);
+}
+
+function plotDailyMeals() {}
